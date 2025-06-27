52f14b576a72bb93a3467004905a656657fbe19d
Andre Carvalho <amc.tomoe@gmail.com>
2025-06-27 14:04:59 -0300

    modificacao: corrigido o arquivo setup.py

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ setup(
     version="0.0.1",
     author=" Andre Martins",
     author_email="andrem.dec@outlook.com",
-    description="Exemplo de pacote Python de processamento de imagem"
+    description="Exemplo de pacote Python de processamento de imagem",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url=https://github.com/Andre-MCarvalho/image-processing-package.git",
+    url="https://github.com/Andre-MCarvalho/image-processing-package.git",
     packages=find_packages(),
     install_requires=requirements,
     python_requires=">=3.6",
